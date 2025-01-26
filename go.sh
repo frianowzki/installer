b1357844cda58a3d58e9f06ea9c55492e43ca069
frianowzki <54317384+frianowzki@users.noreply.github.com>
2025-01-26 14:07:44 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,11 @@
 cd $HOME && \
 ver="1.23.5" && \
+echo -e "Installing Go $GO_VERSION..."
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
 sudo rm -rf /usr/local/go && \
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
 rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
 source ~/.bash_profile && \
+mkdir -p ~/go/bin && \
 go version
