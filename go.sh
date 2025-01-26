cc1f1a008dea9ec6ae3686551b9b190cc18de411
frianowzki <54317384+frianowzki@users.noreply.github.com>
2025-01-26 13:20:05 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,8 +1,11 @@
 wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
+
 sudo rm -rf /usr/local/go
+
 sudo tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
 
 source ~/.bash_profile
+
 mkdir -p ~/go/bin
 
 go version
