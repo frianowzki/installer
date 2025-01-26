8afef6f5e7a455e91c9f7e03c231e9e3d7a05a60
frianowzki <54317384+frianowzki@users.noreply.github.com>
2025-01-26 14:02:15 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,8 +1,9 @@
-wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz
-
-source ~/.bash_profile
-mkdir -p ~/go/bin
-
+cd $HOME && \
+ver="1.22.0" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+source ~/.bash_profile && \
 go version
