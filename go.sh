f289be25335046bddd9f70b87d0f0b14f2437f61
frianowzki <54317384+frianowzki@users.noreply.github.com>
2025-01-26 13:26:44 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,26 +1,8 @@
-printf "Checking latest Go version...\n";
-LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)";
-LATEST_GO_DOWNLOAD_URL="https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
+wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz
 
-printf "cd to home ($USER) directory \n"
-cd $HOME
+source ~/.bash_profile
+mkdir -p ~/go/bin
 
-printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
-curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
-
-printf "Extracting file...\n"
-tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
-
-export GOROOT="$HOME/go"
-export GOPATH="$HOME/go/packages"
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
-printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️
-
-export GOROOT="$HOME/go"
-export GOPATH="$HOME/go/packages"
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-\n'
-
-printf "You are ready to Go!";
 go version
